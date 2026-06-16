d93055b3acb1a7f1143db0ae838f686cdeeba9d2
NadiiaStuzhuk <nadiia.stuzhuk@gmail.com>
2026-06-16 04:17:44 +0300

    Exercise 1: Hello World

--- Week1/Day1/ExercisesXP/ExercisesXP.py
+++ Week1/Day1/ExercisesXP/ExercisesXP.py
@@ -1,6 +1,7 @@
 #Exercise 1: Hello World
 #Instructions
 #Print the following output using one line of code:
+from inspect import Traceback
 print("Hello World\n"*4)
 
 
@@ -21,8 +22,26 @@ print("The result of (99**3) * 8 is", result,".")
 #Example:
 #>>> 15 < 8 #False
 
-5 < 3 #False
-3 == 3 #True
-3 == "3" #False
-"3" > 3 #False
-"Hello" == "hello" #False
+5 < 3
+False
+3 == 3
+True
+3 == "3"
+False
+"3" > 3
+
+#Exercise 4: Your computer brand
+#Traceback (most recent call last):
+#File "<python-input-55>", line 1, in <module>
+#"3" > 3
+#TypeError: '>' not supported between instances of 'str' and 'int'
+#"Hello" == "hello"
+#False
+# Create the variable with your computer's brand
+#computer_brand = "Apple"  # You can change "Apple" to "Dell", "HP", "Lenovo", etc.
+# Print the sentence using an f-string
+#print(f"I have a {computer_brand} computer.")
+# Create a variable called computer_brand
+
+computer_brand = "Lenovo"
+print(f"I have a {computer_brand} computer.")
