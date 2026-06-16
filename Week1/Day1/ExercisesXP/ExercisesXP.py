532c91de532f7d6ef4fabf52037e6f1250c3aa47
NadiiaStuzhuk <nadiia.stuzhuk@gmail.com>
2026-06-16 04:05:01 +0300

    Exercise 1: Hello World

--- Week1/Day1/ExercisesXP/ExercisesXP.py
+++ Week1/Day1/ExercisesXP/ExercisesXP.py
@@ -1,6 +1,28 @@
 #Exercise 1: Hello World
 #Instructions
 #Print the following output using one line of code:
-
-
 print("Hello World\n"*4)
+
+
+#Exercise 2: Some Math
+#Instructions
+#Write code that calculates the result of:
+#(99^3)*8 (meaning 99 to the power of 3, times 8).
+
+result = (99**3) * 8
+print(result)
+print("The result of (99**3) * 8 is", result,".")
+
+
+#Exercise 3: What is the output?
+#Instructions
+#Predict the output of the following code snippets:
+#Comment what is your guess, then run the code and compare
+#Example:
+#>>> 15 < 8 #False
+
+5 < 3 #False
+3 == 3 #True
+3 == "3" #False
+"3" > 3 #False
+"Hello" == "hello" #False
