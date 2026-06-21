287793a171ae077449b92d6c8e058b42f36809d9
NadiiaStuzhuk <nadiia.stuzhuk@gmail.com>
2026-06-21 09:36:54 +0300

    ExerciseXP2

--- Week1/Day4/ExercisesXP.py
+++ Week1/Day4/ExercisesXP.py
@@ -1,4 +1,4 @@
-# Exercise 1: Favorite Numbers
+# 🌟Exercise 1: Favorite Numbers
 # Key Python Topics:
 #Sets
 # Adding/removing items in a set
@@ -17,3 +17,15 @@ my_fav_numbers.add(13)
 my_fav_numbers.remove(13)
 friend_fav_numbers = {2, 8, 21}
 our_fav_numbers = my_fav_numbers.union(friend_fav_numbers)
+
+
+# 🌟 Exercise 2: Tuple
+# Key Python Topics:
+# Tuples (immutability)
+# Instructions:
+# Given a tuple of integers, try to add more integers to the tuple.
+# Hint: Tuples are immutable, meaning they cannot be changed after creation. Think about why you can’t add more integers to a tuple.
+print("Tuples are immutable, so we cannot add more integers to the tuple. You can only create a new tuple with the additional integers.")
+tuple_of_integers = (1, 2, 3, 4, 5)
+additional_tuple_integers = (6, 7, 8)
+print(f"tuple_of_integers: {tuple_of_integers} and additional_tuple_integers: {additional_tuple_integers}")
