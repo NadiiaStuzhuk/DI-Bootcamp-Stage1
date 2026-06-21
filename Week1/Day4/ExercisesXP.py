76d519bad45deed5d46e119451e39c84f3403661
NadiiaStuzhuk <nadiia.stuzhuk@gmail.com>
2026-06-21 11:19:46 +0300

    Exercise3

--- Week1/Day4/ExercisesXP.py
+++ Week1/Day4/ExercisesXP.py
@@ -29,3 +29,27 @@ print("Tuples are immutable, so we cannot add more integers to the tuple. You ca
 tuple_of_integers = (1, 2, 3, 4, 5)
 additional_tuple_integers = (6, 7, 8)
 print(f"tuple_of_integers: {tuple_of_integers} and additional_tuple_integers: {additional_tuple_integers}")
+
+
+# 🌟 Exercise 3: List Manipulation
+# Key Python Topics:
+# Lists
+# List methods: append, remove, insert, count, clear
+# Instructions:
+# You have a list: basket = ["Banana", "Apples", "Oranges", "Blueberries"]
+# Remove "Banana" from the list.
+# Remove "Blueberries" from the list.
+# Add "Kiwi" to the end of the list.
+# Add "Apples" to the beginning of the list.
+# Count how many times "Apples" appear in the list.
+# Empty the list.
+# Print the final state of the list.
+basket = ["Banana", "Apples", "Oranges", "Blueberries"]
+basket.remove("Banana")
+basket.remove("Blueberries")
+basket.append("Kiwi")
+basket.insert(0, "Apples")
+apple_count = basket.count("Apples")
+basket.clear()
+print(f"Final state of the list: {basket}")
+
